deed594792c2da94c9ad1246e2cf9e04737e7621
hirodinn <hirebikila1@gmail.com>
2025-12-09 23:54:15 +0300

    add the interactivity of the calculator

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,26 @@
+const display = document.getElementById("display");
+
+function append(value) {
+  if (display.innerText === "0" && value !== ".") {
+    display.innerText = value;
+  } else {
+    display.innerText += value;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = "0";
+}
+
+function deleteLast() {
+  display.innerText = display.innerText.slice(0, -1) || "0";
+}
+
+function calculate() {
+  try {
+    display.innerText = eval(display.innerText);
+  } catch {
+    display.innerText = "Error";
+  }
+}
 
