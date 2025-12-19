aaac8d734540ad9beb48174e0c6f7fa84ea774d5
hirodinn <hirebikila1@gmail.com>
2025-12-19 10:42:30 +0300

    add the initial structure for the frontend

--- todo/frontend/src/App.jsx
+++ todo/frontend/src/App.jsx
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
-  return <div>App</div>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    async function loadTodos() {
+      try {
+        const res = await axios.get("http://localhost:3001/todos");
+        setTodos(res.data);
+      } catch (ex) {
+        console.log(ex);
+      }
+    }
+    loadTodos();
+  }, []);
+
+  return (
+    <div className="min-h-screen box-border bg-custom">
+      <div className="w-[90%] max-w-200 mx-auto min-h-screen box-border pt-16 flex flex-col pb-6">
+        <div className="py-3 px-7 rounded-2xl bg-red-400 text-white">
+          Add Todo
+        </div>
+        <div className="flex-1 overflow-y-scroll scroll-m-0 bg-amber-200 rounded-2xl"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
